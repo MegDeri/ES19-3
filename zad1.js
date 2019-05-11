5956dc796f7baa77726e1727de7aa10d64f68f3c
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-05-11 15:56:30 +0300

    Create zad1 and modify js

---
+++ zad1.js
@@ -0,0 +1,31 @@
+//zad 1
+let gret = 'Hello';
+let name = 'World';
+
+
+console.log(`${gret} ${name}`);
+
+//zad 2
+let multiplay = (a, b = 1) => {return a * b};
+
+multiplay(5);
+multiplay(5, 6);
+
+//zad 3
+const average = args => args.reduce((a,b) => a + b, 0) / args.length
+
+average([2, 3, 4, 5, 6]);
+
+//zad 4
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+const tab1 = [...numbers, 2, 3];
+const average = args => args.reduce((a,b) => a + b, 0) / args.length
+
+average(tab1);
+
+//zad 5 
+
+let tab2 = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , , firstName, , lastName] = tab2;  
+
+console.log(firstName, lastName);
